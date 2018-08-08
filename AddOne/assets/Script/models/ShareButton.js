c20a551dc86c70e32b59a89ac926d0b47830c476
Anter10 <892466942@qq.com>
2018-08-08 18:18:48 +0800

    分享组件的完善
    分享组件的完善

--- AddOne/assets/Script/models/ShareButton.js
+++ AddOne/assets/Script/models/ShareButton.js
@@ -1,12 +1,8 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+ /*
+    游戏分享prefab
+    游戏的分享操作的主要逻辑在这个module里面编写
+    created by gyc on 2018-08-08.
+*/
 
 cc.Class({
     extends: cc.Component,
@@ -15,21 +11,95 @@ cc.Class({
       
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
+      /*
+        调用: 场景加载完成后的回调
+        功能: 场景加载完成后的一些UI逻辑处理
+        参数: [
+            无
+        ]
+        返回值:[
+            无
+        ]
+        思路: 系统自带
+    */
     onLoad () {
         this.data = {};
     },
-
+    /*
+        调用: 组件开始调用的时候调用
+        功能: 组件开始调用的时候的逻辑处理
+        参数: [
+            无
+        ]
+        返回值:[
+            无
+        ]
+        思路: 系统自带
+    */
     start () {
 
     },
-
-    setShareData:function(){
+    
+    /*
+        调用: 使用此model分享功能的时候 手动调用刷新分享的数据
+        功能: 设置即将要分享的数据
+        参数: [
+            data: 分享的数据
+        ]
+        返回值:[
+            无
+        ]
+        思路: 系统自带
+    */
+    setShareData:function(data){
         this.data = data;
     },
 
+    /*
+        调用: 使用此model分享功能的时候 
+        功能: 手动设置分享成功后的回调函数
+        参数: [
+            successCall: 分享成功后的回调方法 类型Function
+        ]
+        返回值:[
+            无
+        ]
+        思路: 逻辑需要
+    */
+    setSuccessCall: function(successCall){
+        this.successCallBack = successCall;
+    },
+
+     /*
+        调用: 使用此model分享功能的时候 
+        功能: 手动设置分享失败后的回调函数
+        参数: [
+            errorCall: 分享失败后的回调方法 类型Function
+        ]
+        返回值:[
+            无
+        ]
+        思路: 逻辑需要
+    */
+    setErrorCall: function(errorCall){
+        this.successCallBack = errorCall;
+    },
+    
+
+
+    /*
+        调用: 使用此model分享功能的时候 
+        功能: 给好友或者群分享小程序的相关信息
+        参数: [
+            无
+        ]
+        返回值:[
+            无
+        ]
+        思路: 逻辑需要
+    */
     shareMiniApp:function(){
+        var self = this;
         if(tywx.IsWechatPlatform()){
             window.wx.showShareMenu({withShareTicket: true});
             var msg = tywx.ShareInterface.getRandomOnShareAppMessageInfo();
@@ -43,11 +113,14 @@ cc.Class({
                     (res) => {
                         console.log("分享成功后的数据"+JSON.stringify(res));
                         if (res.shareTickets != undefined && res.shareTickets.length > 0) {
-                            window.wx.postMessage({
-                                method: 2,
-                                MAIN_MENU_NUM: "x1",
-                                shareTicket: res.shareTickets[0]
-                            });
+                            // window.wx.postMessage({
+                            //     method: 2,
+                            //     MAIN_MENU_NUM: "x1",
+                            //     shareTicket: res.shareTickets[0]
+                            // });
+                            if(self.successCall){
+                                self.successCall(res);
+                            }
                         }
                     },null,
                     function(data){
