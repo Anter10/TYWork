9186dd3191eacf4c37472c7624029309717f7dc2
Anter10 <892466942@qq.com>
2018-08-10 15:13:49 +0800

    部分功能更改

--- AddOne/build/wechatgame/main.13ce0.js
+++ AddOne/build/wechatgame/main.13ce0.js
@@ -166,7 +166,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.affdb.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.13ce0.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -190,7 +190,7 @@ if (false) {
     window.boot();
 }
 else if (window.jsb) {
-    require('src/settings.75deb.js');
+    require('src/settings.f119e.js');
     require('src/cocos2d-jsb.js');
     require('jsb-adapter/engine/index.js');
     window.boot();
